7a67199f4681a692b3c3d882b7a613b362378e00
maxi <eulamaxi@gmail.com>
2024-11-06 17:18:56 -0300

    ...

--- app/micro_services/producto/producto.py
+++ app/micro_services/producto/producto.py
@@ -5,22 +5,21 @@ import os
 app = Flask(__name__)
 
 #Configuración de la conexión a la base de datos usando variables de entorno
-# DATABASE_USER = os.getenv("POSTGRES_USER")
-# DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-# DATABASE_HOST = os.getenv("POSTGRES_HOST")
-# DATABASE_PORT = os.getenv("POSTGRES_PORT")
-# DATABASE_NAME = os.getenv("POSTGRES_DB")
-
+DATABASE_USER = os.getenv("POSTGRES_USER")
+DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DATABASE_HOST = os.getenv("POSTGRES_HOST")
+DATABASE_PORT = os.getenv("POSTGRES_PORT")
+DATABASE_NAME = os.getenv("POSTGRES_DB")
 
 # Función para conectar a la base de datos
 def get_db_connection():
     try:
         conn = psycopg2.connect(
-            host="ecomercedb-servidor",
-            database="ecomercedb",
-            user="admin",
-            password="1234",
-            port="5432"
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD,
+            port=DATABASE_PORT
         )
         return conn
     except psycopg2.OperationalError as e:
@@ -28,25 +27,25 @@ def get_db_connection():
         return None
 
 
-# Endpoint para obtener todos los stock
-@app.route('/stock', methods=['GET'])
-def get_stock():
+# Endpoint para obtener todos los productos
+@app.route('/productos', methods=['GET'])
+def get_productos():
     conn = get_db_connection()
     if conn is None:
         return jsonify({"error": "No se pudo conectar a la base de datos."}), 500
     
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM stock;")
-    stock = cursor.fetchall()
+    cursor.execute("SELECT * FROM productos;")
+    productos = cursor.fetchall()
     cursor.close()
     conn.close()
 
-    # Convertir los stock en un formato JSON
-    if not stock:
-        return jsonify([])  # Devuelve una lista vacía si no hay stock
+    # Convertir los productos en un formato JSON
+    if not productos:
+        return jsonify([])  # Devuelve una lista vacía si no hay productos
 
-    stock_list = [{"id": p[0], "nombre": p[1], "precio": p[2]} for p in stock]
-    return jsonify(stock_list)
+    productos_list = [{"id": p[0], "nombre": p[1], "precio": p[2]} for p in productos]
+    return jsonify(productos_list)
 
 # Ruta de prueba para verificar la conexión
 @app.route('/ping', methods=['GET'])
